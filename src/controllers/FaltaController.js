581dc235152090a7acea77891c679dc54cbba09c
VitorAK <vitor.akira.uehara@gmail.com>
2024-04-22 21:48:24 -0300

    22/04

--- src/controllers/FaltaController.js
+++ src/controllers/FaltaController.js
@@ -30,46 +30,6 @@ module.exports = {
     }
   }
 
-  /*registrarFaltas: async (faltasEnviar) => {
-    try {
-      // Mapeia os códigos dos alunos para objetos que serão inseridos na tabela de faltas
-      const registrosFaltas = faltasEnviar.map(async (falta) => {
-        const { codAluno } = falta;
-        // Aqui você pode adicionar lógica para verificar se o aluno já possui registros de falta para a data atual,
-        // ou qualquer outra lógica específica do seu sistema
-        const dataAtual = new Date().toISOString().split('T')[0];
-
-        // Cria um registro de falta para o aluno na data atual
-        const registroFalta = await db.Falta.create({
-          Cod_Aluno: codAluno,
-          Qtde_Faltas: 1, // Por exemplo, inicia a quantidade de faltas como 1
-          Datas: dataAtual
-        });
-
-        return registroFalta;
-      });
-
-      // Aguarda a conclusão de todas as inserções de falta
-      const result = await Promise.all(registrosFaltas);
-
-      return result;
-    } catch (error) {
-      throw new Error('Erro ao registrar faltas: ' + error.message);
-    }
-  },
-
-  confirmarFalta: async (req, res) => {
-    try {
-      const { faltasEnviar } = req.body; // Recebe os códigos dos alunos que faltaram do corpo da requisição
-      const registrosFaltas = await FaltaService.registrarFaltas(faltasEnviar); // Chama o serviço para registrar as faltas no banco de dados
-
-      res.status(200).json({ message: 'Faltas confirmadas com sucesso!', registrosFaltas });
-    } catch (error) {
-      console.error('Erro ao confirmar faltas:', error);
-      res.status(500).json({ error: 'Erro ao confirmar faltas. Verifique o console para mais detalhes.' });
-    }
-  }*/
-
 };
 
 
